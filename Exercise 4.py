101139405a75f5765e374c3e1718cfd866c581c9
MaximilianSoeren <74557518+MaximilianSoeren@users.noreply.github.com>
2020-11-17 12:37:04 +0000

    Add files via upload

--- Exercise 4.py
+++ Exercise 4.py
@@ -1,7 +1,7 @@
 # import all our modules
 import openpyxl
 from os import path
-
+from openpyxl import cell
 # # Trying to program a login program
 
 # Write different functions used in the code.
@@ -19,22 +19,28 @@ def load_workbook(wb_path):
 wb_path = "userdata.xlsx"
 wb = load_workbook(wb_path)
 
-ws = wb["userdata"]
+# ws = wb["userdata"]
+ws = wb.active
+
+
 # Define home screen, the user is redirected to this in case of an error.
-# def home_screen():
-#     print("Please select if you want to create an account or log in.")
-#     print("(1): Create an account.""\n" "(2): Log in.""\n" "(3) Show Usernames.""\n" "(4) Show Passwords.")
-#     print("\n")
-#     selection = int(input("Please enter your selection: "))
-#     if selection == 1:
-#         create_account()
-#     elif selection == 2:
-#         log_in()
-#     elif selection == 3:
-#         print(usernames)
-#     elif selection == 4:
-#         print(passwords)
-#
+def home_screen():
+    print("Please select if you want to create an account or log in.")
+    print("(1): Create an account.""\n" "(2): Log in.""\n" "(3): Show Usernames.""\n" 
+          "(4): Show Passwords.""\n" "(5): Exit.")
+    print("\n")
+    selection = int(input("Please enter your selection: "))
+    if selection == 1:
+        create_account()
+    elif selection == 2:
+        log_in()
+    elif selection == 3:
+        print(usernames)
+    elif selection == 4:
+        print(passwords)
+    elif selection == 5:
+        exit()
+
 
 usernames = []
 passwords = []
@@ -50,37 +56,33 @@ def create_account():
         print("This username is already taken")
     else:
         usernames.append(username)
-
-    # passwords.append(password)
+    passwords.append(password)
     for username in usernames:
-        row = 1
-        if ws.cell.value is None:
-            ws.cell(row=row, column=1, value=username)
-        else:
-            row += 1
+        ws.insert_rows(1)
+        ws.cell(row=1, column=1, value=username)
+    for password in passwords:
+        ws.cell(row=1, column=2, value=password)
+    wb.save(wb_path)
+    log_in()
 
-    # for password in passwords:
-    #     sheet.cell(sheet, column=2, value=password)
 
-# def log_in():
-#     userdata = open("data.txt", "r")
-#     username = ("\n" + input("Please enter your username: "))
-#     password = ("\n" + input("Please enter your password: "))
-#     if username ==
+def log_in():
+    print("Let's log in, please enter your data beneath""\n")
+    username = ("\n" + input("Please enter your username: "))
+    password = ("\n" + input("Please enter your password: "))
+    if username == ws.values([usernames]) and password == ws.values([passwords]):
+        print("You have successfully logged in")
+        home_screen()
 
 
 
 # creating usernames and password lists.
 
-usernames = []
-passwords = []
 
 # getting input from the user to put into the lists
 
 
-create_account()
-
-#
+home_screen()
 
 #
 #
@@ -96,4 +98,4 @@ create_account()
 #     Print("Please select one either (1) or (2).")
 
 
-wb.save(wb_path)
+
