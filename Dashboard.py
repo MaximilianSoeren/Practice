0a3d1d04ce26fbe7c0e9294996416faa3c15f259
MaximilianSoeren <74557518+MaximilianSoeren@users.noreply.github.com>
2020-11-21 10:59:59 +0100

    Add files via upload

---
+++ Dashboard.py
@@ -0,0 +1,25 @@
+# This is a program to make a dashboard as a Homescreen.
+# This will be a dashboard where it shows Stocks / Spotify / Weather / To Do list / Meetings ( Calendar) / E-mails
+# all in once place. This proejct will most likely be a bigger one so I will put it in smaller projects and combine them
+# afterwards.
+## Overall arching Step structure:
+#   Step1: Figure out what Services I want to use.
+#   Step2: Figure out how to interact with those services (API / CSV...)
+#   Step3: Go Step by step through the programs and figure out how to pull the information out of each one.
+#   Step4: Figure out a layout to display it.
+
+# Step1:
+#       Spotify (via API, probably just displaying but maybe interaction?)
+#       Gmail / Zohoo
+#       Open source weather app (API compatible)
+#       CVS Stocks (always show yesterday or figure a way out how to update it automatically (maybe screen grabber?)
+#       Figure out how you can interact with notion.so / otherwise do a to do list in txt file / print.
+#       Calendar app / preferably notion.so but could also be something else
+
+# Step2:
+#       Spotfiy:
+#       Questions:
+#       Figure out if interaction is possible or if it's too hard for now.
+#       If too hard --> just display current song / maybe leave room for the small display thingy.
+#       Interacting with API or go via Webbrowser app?
+
